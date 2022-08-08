c05dad49364a3fadbbbeee5e44282c57ed2e5558
Paulo Almeida <paulo.roberto.a@hotmail.com>
2022-08-08 22:49:26 +0200

    My Lab

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld  />
+    <Goodbye />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import Goodbye from "./components/GoodBye.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Goodbye,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/GoodBye.vue
@@ -0,0 +1,51 @@
+<template>
+  <div v-for="(post, index) in posts" :key="index">
+    <h1>{{ post.name }}</h1>
+    My age is {{ post.age }} <br />
+    {{ post.comment }} <br />
+    {{ injected }} <br />
+    <span v-html="html"></span>
+    {{ fruta }}
+    <br />
+    <span v-if="post.age >= 21 && canDrink"> 'Puedo beber'</span>
+    <span v-else>'No puedo beber'</span>
+
+    <button @click="count++">count is: {{ count }}</button>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      injected: "me encanta las manzanas",
+      posts: [
+        {
+          name: "Paulo",
+          age: 35,
+          comment: "Soy Brasileño y vivo en Barcelona desde 2018",
+        },
+        {
+          name: "Ana",
+          age: 16,
+          comment: "Portuguesa y vive en Copenhagen desde 2020",
+        },
+        {
+          name: "Luisa",
+          age: 32,
+          comment: "Francesa y vive en Bahamas desde 2008",
+        },
+      ],
+      html: `<p>mi pelicula favorita es Vanilla Sky</p>`,
+      canDrink: true,
+      cantDrink: "No puede beber",
+      count: 0,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
